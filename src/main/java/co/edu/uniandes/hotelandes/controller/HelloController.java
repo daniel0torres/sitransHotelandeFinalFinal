ae930da9f54cb280fddc66a3881c6667e6265f95
daniel felipe torres lopez <d.torresl@uniandes.edu.co>
2023-11-28 20:50:37 -0500

    CRUD del hola

--- src/main/java/co/edu/uniandes/hotelandes/controller/HelloController.java
+++ src/main/java/co/edu/uniandes/hotelandes/controller/HelloController.java
@@ -1,43 +1,69 @@
 package co.edu.uniandes.hotelandes.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import co.edu.uniandes.hotelandes.model.Hello;
 import co.edu.uniandes.hotelandes.repository.HelloRepository;
-import java.util.List;
 import org.bson.types.ObjectId;
-import org.springframework.data.repository.query.Param;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
-@RestController
+@Controller
 public class HelloController {
 
-    
-    
     @Autowired
     HelloRepository helloRepository;
+
     
+    @GetMapping("/hellos")
+    public String read(Model model){
+        model.addAttribute("hellos", helloRepository.findAll());
+        return "hellos";
+    }
+    @GetMapping("/hellos/new")
+    public String create(Model model) {
+        model.addAttribute("hello", new Hello());
+        return "helloNuevo";
+    }
+
+    @PostMapping("/hellos/new/save")
+    public String createSave(@ModelAttribute Hello hello) {
+        helloRepository.insert(new Hello(hello.getName(),hello.getEdad()));
+
+        return "redirect:/hellos";
+    }
     
+    @GetMapping("/hellos/{id}/edit")
+    public String update(@PathVariable String id, Model model) {
+        ObjectId objectId = new ObjectId(id);
+        Hello hello = helloRepository.findById(objectId).orElse(null);
+        if (hello != null) {
+            model.addAttribute("hello", new Hello());
+            return "helloEditar";
+        }
 
-
-    @GetMapping(value = "/hello2")
-    public List<Hello> index2() {
-
-        List<Hello> l = helloRepository.findAll();
-        return l;
+        else {
+            return "redirect:/hellos";
+        }
 
     }
     
-    @GetMapping(value = "/create")
-    public Hello crearHello(@Param(value = "name")String name, @Param(value = "edad") int edad){
-        Hello h = helloRepository.insert(new Hello(name,edad));
-        return h;
+    @PostMapping("/hellos/{id}/edit/save")
+    public String updateSave(@PathVariable("id") String id, @ModelAttribute Hello hello) {
+       ObjectId objectId = new ObjectId(id);
+       hello.setId(objectId);
+        helloRepository.save(hello);
+        
+        return "redirect:/hellos";
     }
-    @GetMapping(value = "/find")
-    public Hello crearHello(@Param(value = "id")String id){
-        Hello hello = helloRepository.findById(new ObjectId(id)).orElse(null);
-        return hello;
+    
+    @GetMapping("/hellos/{id}/delete")
+    public String delete(@PathVariable String id) {
+        ObjectId objectId = new ObjectId(id);
+        helloRepository.deleteById(objectId);
+        return "redirect:/hellos";
     }
 }
-
