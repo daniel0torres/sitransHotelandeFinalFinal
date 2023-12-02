214c5eb0079eafcf074c1e711293ea8bda515638
David Zamora <d.zamorac@uniandes.edu.co>
2023-12-02 16:09:23 -0500

    alojamientos funcionando

--- src/main/java/co/edu/uniandes/hotelandes/controller/AlojamientoController.java
+++ src/main/java/co/edu/uniandes/hotelandes/controller/AlojamientoController.java
@@ -12,6 +12,7 @@ import java.util.Optional;
 
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -93,7 +94,7 @@ public class AlojamientoController {
     }
     
     @PostMapping("/alojamientos/{id}/edit/save")
-    public String updateSave(@PathVariable("id") String id,  @RequestParam Date fechaLlegada, @RequestParam Date fechaSalida, @RequestParam String habitacion) {
+    public String updateSave(@PathVariable("id") String id,  @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") Date fechaLlegada, @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") Date fechaSalida, @RequestParam String habitacion) {
         ObjectId objectId = new ObjectId(id);
         Optional<Alojamiento> alojamientoOptional = alojamientoRepository.findById(objectId);
         Alojamiento alojamiento = alojamientoOptional.orElse(null);
